b46ce14b65b92e8cb89ca025547a9c7ff03bdfb8
Chris Alberti <christopherjalberti@gmail.com>
2018-05-16 13:46:14 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+  attr_reader :word
+  def initialize(word)
+    @word = word
+    @sorted_word = word.split("").sort
+  end
+
+  def match(words)
+    result = words.select do |word|
+      sorted_check = word.split("").sort
+      @sorted_word == sorted_check
+    end
+    result
+  end
+end
