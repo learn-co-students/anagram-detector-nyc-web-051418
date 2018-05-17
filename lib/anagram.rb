6eacf000b214e1065b0fd6e7a505bd45b55e7b3a
ho-tonym <ho.tonym@gmail.com>
2018-05-17 17:19:18 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+    @fragmented_word = word.split("").sort
+  end
+
+  def match(words)
+    final = words.select do |word|
+      sorted_check = word.split("").sort
+      @fragmented_word == sorted_check
+          #  binding.pry
+    end
+    final
+  end
+end
