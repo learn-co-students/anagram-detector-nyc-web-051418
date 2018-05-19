0b95eb1257372e0b1299d2ce68a1f2d6ef536d35
github username <github email address>
2018-05-19 17:07:16 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+    # binding.pry
+  end
+
+  def match(phrase)
+    phrase.select do |phrase_word|
+      # first check number of letters match
+      if word.length == phrase_word.length
+        # sort the letters and see if they're an exact match
+        if word.split("").sort == phrase_word.split("").sort
+          phrase_word
+        end
+      end  
+    end
+  end
+
+end
